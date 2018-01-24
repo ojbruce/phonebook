f607b48b318f2940a2aae2edfb1ba9c7b230626a
ojbruce <olivia.j.bruce@gmail.com>
2018-01-24 22:34:17 +0100

    Adding  contact reducers & test

---
+++ src/reducers/reducers.js
@@ -0,0 +1,35 @@
+import { ADD_CONTACT, DELETE_CONTACT, EDIT_CONTACT_NAME, EDIT_CONTACT_NUMBER } from '../constants/action-types'
+
+function contacts (state = [], action) {
+  switch (action.type) {
+    case ADD_CONTACT:
+      return [
+        ...state,
+        {
+          id: state.reduce((maxId, contact) => Math.max(contact.id, maxId), -1) + 1,
+          name: action.name,
+          phoneNumber: action.phoneNumber
+        }
+      ]
+    case DELETE_CONTACT:
+      return state.filter(contact => contact.id !== action.id)
+    case EDIT_CONTACT_NAME:
+      return state.map(
+        contact =>
+          contact.id === action.id
+            ? { ...contact, name: action.name }
+            : contact
+      )
+    case EDIT_CONTACT_NUMBER:
+      return state.map(
+        contact =>
+          contact.id === action.id
+            ? { ...contact, phoneNumber: action.phoneNumber }
+            : contact
+      )
+    default:
+      return state
+  }
+}
+
+export default contacts
---
+++ src/reducers/reducers.spec.js
@@ -0,0 +1,85 @@
+import contacts from './reducers'
+
+const chai = require('chai')
+const expect = chai.expect
+
+describe('Contacts reducer', () => {
+  it('should handle initial state', () => {
+    expect(contacts(undefined, {})).to.deep.equal([])
+  })
+
+  it('should handle ADD_CONTACT', () => {
+    expect(
+      contacts([], {
+        type: 'ADD_CONTACT',
+        name: 'Name Name',
+        phoneNumber: '0697656565'
+      })
+    ).to.deep.equal([
+      {
+        id: 0,
+        name: 'Name Name',
+        phoneNumber: '0697656565'
+      }
+    ])
+  })
+
+  it('should handle EDIT_CONTACT_NAME', () => {
+    expect(
+      contacts([
+        {
+          id: 0,
+          name: 'Name Name',
+          phoneNumber: '0697656565'
+        }
+      ], {
+        type: 'EDIT_CONTACT_NAME',
+        id: 0,
+        name: 'New Name'
+      })
+    ).to.deep.equal([
+      {
+        id: 0,
+        name: 'New Name',
+        phoneNumber: '0697656565'
+      }
+    ])
+  })
+
+  it('should handle EDIT_CONTACT_NUMBER', () => {
+    expect(
+      contacts([
+        {
+          id: 0,
+          name: 'Name Name',
+          phoneNumber: '0697656565'
+        }
+      ], {
+        type: 'EDIT_CONTACT_NUMBER',
+        id: 0,
+        phoneNumber: '0606060606'
+      })
+    ).to.deep.equal([
+      {
+        id: 0,
+        name: 'Name Name',
+        phoneNumber: '0606060606'
+      }
+    ])
+  })
+
+  it('should handle DELETE_CONTACT', () => {
+    expect(
+      contacts([
+        {
+          id: 0,
+          name: 'New Name',
+          phoneNumber: '0697656565'
+        }
+      ], {
+        type: 'DELETE_CONTACT',
+        id: 0
+      })
+    ).to.deep.equal([])
+  })
+})
