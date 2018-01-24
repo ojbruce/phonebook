fbaeb57971c0459bbd121747158643f335511de3
ojbruce <olivia.j.bruce@gmail.com>
2018-01-24 22:51:35 +0100

    Adding some vars and style

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers/reducers'
+
+import App from './containers/app'
+require('./style/base.scss')
+
+const store = createStore(reducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
